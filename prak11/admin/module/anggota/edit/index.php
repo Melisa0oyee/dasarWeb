fe266ee980991bd8d8f2fdea21207d3a4339436e
Nur Melisa <nur0melisaoyee@gmail.com>
2024-04-26 02:22:58 +0700

    pertemuan 11 - praktikum 1-5

---
+++ prak11/admin/module/anggota/edit/index.php
@@ -0,0 +1,124 @@
+<head>
+    <!-- Masukkan CSS lainnya di sini -->
+    <style>
+        .flex-footer {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #0D6EFD; /* Ganti dengan warna latar belakang yang diinginkan */
+            text-align: center;
+            padding: 5px 0; /* Sesuaikan padding atas dan bawah */
+        }
+
+        .flex-footer b {
+            color: whitesmoke; /* Ganti dengan kode warna yang diinginkan, misalnya #ff0000 untuk merah */
+        }
+    </style>
+</head>
+<div class="container-fluid">
+    <div class="row">
+        <?php
+        require 'admin/template/menu.php';
+        $id = $_GET['id'];
+        $query = "SELECT * FROM anggota a, jabatan j, user u WHERE a.jabatan_id = j.id AND a.user_id = u.id AND a.user_id = '$id'";
+        $result = mysqli_query($koneksi, $query);
+        $row = mysqli_fetch_assoc($result);
+        ?>
+
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">Anggota</h1>
+            </div>
+            <form action="fungsi/edit.php?anggota=edit" method="POST">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-header">Form Edit Anggota</div>
+                            <div class="card-body">
+                                <input type="hidden" value="<?php echo $row['user_id']; ?>" name="id">
+                                <div class="mb-3">
+                                    <label for="nama" class="form-label">Nama</label>
+                                    <input type="text" class="form-control" name="nama" value="<?= $row['nama']; ?>">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="jabatan" class="form-label">Jabatan:</label>
+                                    <select class="form-select" name="jabatan" aria-label="Default select example">
+                                        <option selected>Pilih Jabatan</option>
+                                        <?php
+                                        $query2 = "SELECT * FROM jabatan order by jabatan asc";
+                                        $result2 = mysqli_query($koneksi, $query2);
+                                        while ($row2 = mysqli_fetch_assoc($result2)) {
+                                            ?>
+                                            <option value="<?= $row2['id']; ?>" <?= ($row['jabatan_id'] == $row2['id']) ? 'selected' : '' ?>>
+                                                <?= $row2['jabatan']; ?>
+                                            </option>
+                                        <?php }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Jenis Kelamin:</label>
+                                    <br>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="jenis_kelamin" value="L" <?= ($row['jenis_kelamin'] === "L") ? 'checked' : '' ?>>
+                                        <label class="form-check-label" for="inlineRadio1">Laki-Laki</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="jenis_kelamin" value="P" <?= ($row['jenis_kelamin'] == "P") ? 'checked' : '' ?>>
+                                        <label class="form-check-label" for="inlineRadio2">Perempuan</label>
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="alamat" class="form-label">Alamat</label>
+                                    <textarea class="form-control" name="alamat"><?= $row['alamat']; ?></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="no_hp" class="form-label">No Telepon</label>
+                                    <input type="number" class="form-control" name="no_hp" value="<?= $row['no_hp']; ?>">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-header">
+                                Form Edit Login Anggota
+                            </div>
+                            <div class="card-body">
+                                <div class="mb-3">
+                                    <label for="username" class="form-label">Username</label>
+                                    <input type="text" class="form-control" name="username" value="<?= $row['username']; ?>">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password</label>
+                                    <input type="password" class="form-control" name="password">
+                                    <div class="form-text">Kosongkan password jika tidak ingin menggantinya.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-body" style="text-align: center;">
+                                <button type="submit" class="btn btn-primary"><i class="fa fa-floppy-o"
+                                        aria-hidden="true"></i> Ubah</button>
+                                <a href="index.php?page=anggota" class="btn btn-secondary"><i class="fa fa-times"
+                                        aria-hidden="true"></i> Batal</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </main>
+    </div>
+</div>
+<div class="flex-footer">
+        &copy; 2024. <b>Ritech.id</b> all rights reserved
+    </div>
